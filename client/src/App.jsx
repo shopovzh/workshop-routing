a3bbfc85ec15ec306176236d978630dc564aa78d
Zhivko Shopov <shopovzh@gmail.com>
2024-07-23 16:40:33 +0100

    Install react-router and add route for home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div id="box">
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
