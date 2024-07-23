d388f4ce76cb1760685b0bae14b0d787644ae764
Zhivko Shopov <shopovzh@gmail.com>
2024-07-23 16:15:56 +0100

    Added header component and app styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,11 @@
-function App() {}
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <div id="box">
+      <Header></Header>
+    </div>
+  );
+}
 
 export default App;
