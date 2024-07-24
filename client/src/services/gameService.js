1d1e92c38aeba8271981238d8113c14d4662557b
Zhivko Shopov <shopovzh@gmail.com>
2024-07-24 10:04:19 +0100

    Add request for all games

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,5 +1,19 @@
 const baseUrl = "http://localhost:3030/jsonstore";
 
+export const getAllGames = async () => {
+  try {
+    const response = await fetch(`${baseUrl}/games`);
+
+    if (response.ok == false) {
+      throw new Error("Request failed!");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const create = async (gameData) => {
   try {
     const response = await fetch(`${baseUrl}/games`, {
